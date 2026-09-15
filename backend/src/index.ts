beccebb402dd7af119e561a3a3edd8b605d96c0c
Hussein <deebh776@gmail.com>
2026-09-15 18:16:29 +0300

    Config render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import authRoutes from './routes/auth.route';
-import corsOptions from './configs/corsOptions';
-import connectDB from './configs/connectDb';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.route";
+import corsOptions from "./configs/corsOptions";
+import connectDB from "./configs/connectDb";
 
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 connectDB();
@@ -19,8 +21,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Routes
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
